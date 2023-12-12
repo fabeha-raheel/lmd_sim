c0669b261cc568501c7475eff716a32fb1fe1db6
fabeha-raheel <fabeha.raheel.aeelab@gmail.com>
2023-12-12 17:40:36 +0300

    saved data to logs to test offline data logging and mapping

---
+++ scripts/offline_mapping.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import tkinter as tk
+import tkintermapview
+from tkinter import ttk
+import pickle
+
+import drone_data
+
+MAPPING_LOG = '/home/df/lmd_ws/src/lmd_sim/logs/lmd_data.txt'
+
+class MappingApp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.title('Landmine Mapping GUI')
+        self.geometry('500x500')
+
+        self.locations = drone_data.landmines
+        self.inital_zoom = 20
+        self.initial_position = (-35.3632621, 149.1652374)
+        self.markers = []
+
+        self.labelFrame = tk.LabelFrame(self)
+        self.labelFrame.pack(pady=20)
+
+        self.map_widget = tkintermapview.TkinterMapView(self.labelFrame, width=1000, height=700, corner_radius=0)
+        self.map_widget.pack(fill="both", expand=True)
+        self.map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=22)  # google satellite
+        # self.map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=22)  # google normal
+        self.map_widget.set_position(self.initial_position[0], self.initial_position[1])
+        self.map_widget.set_zoom(self.inital_zoom)
+
+        self.create_map()
+    
+    def create_map(self):
+
+        self.read_data()
+        for location in self.locations:
+            index = self.locations.index(location)
+            text = "Landmine " + str(index + 1)
+            self.markers.append(self.map_widget.set_marker(location[1][0], location[1][1], text=text))
+    
+    def read_data(self):
+        f = open(MAPPING_LOG, 'r')
+        data = list(f.read())
+        print("Mapping Landmine : ", data)
+        self.locations = data
+        f.close()
+
+root = MappingApp()
+root.mainloop()
